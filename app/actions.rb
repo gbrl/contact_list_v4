bd34108d9fda9e7ee8c82b2ec0352ff4219c576a
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-01 12:16:46 -0400

    Add basic API routes

--- app/actions.rb
+++ app/actions.rb
@@ -9,3 +9,29 @@ end
 get '/' do
   erb :index
 end
+
+namespace '/api' do
+  namespace '/v1' do
+
+    get '/' do
+      json({ message: "Welcome to the Contact List API" })
+    end
+
+    get '/create' do
+      json({ message: "You've reached the /create endpoint." })
+    end
+
+    get '/update' do
+      json({ message: "You've reached the /update endpoint." })
+    end
+
+    get '/destroy' do
+      json({ message: "You've reached the /destroy endpoint." })
+    end
+
+    get '/list' do
+      json({ message: "You've reached the /list endpoint." })
+    end
+
+  end
+end
